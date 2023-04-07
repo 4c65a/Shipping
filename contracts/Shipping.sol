b79fa68bf94d290ac4ea3d676ac3c08567c4ddd0
LeanBorquez <leandroborqueza@gmail.com>
2023-04-07 18:59:31 -0300

    Smart

--- contracts/Shipping.sol
+++ contracts/Shipping.sol
@@ -1,6 +1,13 @@
 pragma solidity ^0.8.9;
 
+
+/**
+ * @title Shipping
+ * @dev Un contrato para registrar el estado de envío de un paquete.
+ */
+
 contract Shipping {
+
     enum ShippingStatus {
         Pending, //Pendiente
         Shipped, // Enviado
@@ -11,20 +18,34 @@ contract Shipping {
 
     event LogNewAlert(string description);
 
+    /**
+     * @dev Constructor que establece el estado inicial en "Pendiente".
+     */
     constructor() public {
         status = ShippingStatus.Pending;
     }
 
+     /**
+     * @notice Actualiza el estado de envío a "Enviado".
+     */
     function Shipped() public {
         status = ShippingStatus.Shipped;
         emit LogNewAlert("Your package has been shipped");
     }
 
+    /**
+     * @notice Actualiza el estado de envío a "Entregado".
+     */
     function Delivered() public {
         status = ShippingStatus.Delivered;
         emit LogNewAlert("Your package has arrived");
     }
 
+    /**
+    * @dev Devuelve el estado de envío actual en formato legible.
+    * @param _status El estado de envío actual.
+    * @return El estado de envío actual.
+     */
     function getStatus(ShippingStatus _status)
         internal
         pure
@@ -37,6 +58,10 @@ contract Shipping {
         if (ShippingStatus.Delivered == _status) return "Delivered";
     }
 
+    /**
+     * @notice Devuelve el estado de envío actual en formato legible.
+     * @return El estado de envío actual.
+     */
     function Status() public view returns (string memory) {
         ShippingStatus _status = status;
         return getStatus(_status);
